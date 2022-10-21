8bb56d1780460bc384de244838ab9d51a253b1b7
Danish-Asghar2909 <61777716+Danish-Asghar2909@users.noreply.github.com>
2022-10-21 11:52:50 +0530

    Updates

---
+++ precache-manifest.26e94c9111ca97cd0dd2f521b88c397f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa8d84aec68be7e6a3c52d9d388627c",
+    "url": "/telegram-cloce-v2/index.html"
+  },
+  {
+    "revision": "9eec42147ed5d2ccb7c0",
+    "url": "/telegram-cloce-v2/static/css/0.9b2f7aac.chunk.css"
+  },
+  {
+    "revision": "70b7d65df986a044278a",
+    "url": "/telegram-cloce-v2/static/css/4.ac920775.chunk.css"
+  },
+  {
+    "revision": "f1d0bc580a2176ee87c0",
+    "url": "/telegram-cloce-v2/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "feb77a3ce4ce7d58742d",
+    "url": "/telegram-cloce-v2/static/css/7.4c27677e.chunk.css"
+  },
+  {
+    "revision": "57f85577d8909790b7cd",
+    "url": "/telegram-cloce-v2/static/css/main.a1c553f4.chunk.css"
+  },
+  {
+    "revision": "9eec42147ed5d2ccb7c0",
+    "url": "/telegram-cloce-v2/static/js/0.0f030d06.chunk.js"
+  },
+  {
+    "revision": "f77df77b0064a06a0ced",
+    "url": "/telegram-cloce-v2/static/js/3.c014426f.chunk.js"
+  },
+  {
+    "revision": "70b7d65df986a044278a",
+    "url": "/telegram-cloce-v2/static/js/4.f18c15a9.chunk.js"
+  },
+  {
+    "revision": "f1d0bc580a2176ee87c0",
+    "url": "/telegram-cloce-v2/static/js/5.70dd5907.chunk.js"
+  },
+  {
+    "revision": "7d90f731adb96c7b86e3",
+    "url": "/telegram-cloce-v2/static/js/6.91934930.chunk.js"
+  },
+  {
+    "revision": "feb77a3ce4ce7d58742d",
+    "url": "/telegram-cloce-v2/static/js/7.9c9edff2.chunk.js"
+  },
+  {
+    "revision": "57f85577d8909790b7cd",
+    "url": "/telegram-cloce-v2/static/js/main.3b2c2699.chunk.js"
+  },
+  {
+    "revision": "660512ef3713265c0bee",
+    "url": "/telegram-cloce-v2/static/js/runtime~main.91d4e168.js"
+  },
+  {
+    "revision": "c6a92d89aa71027ea621d52bb03ad681",
+    "url": "/telegram-cloce-v2/static/media/bubble-tail-left.c6a92d89.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/telegram-cloce-v2/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "fb38f7d6fbf79f207edfa158c10f16a7",
+    "url": "/telegram-cloce-v2/static/media/check.fb38f7d6.svg"
+  },
+  {
+    "revision": "8e3f975b0dac0d7ddc9f5ab85f24d15a",
+    "url": "/telegram-cloce-v2/static/media/telegram-logo.8e3f975b.svg"
+  }
+]);
